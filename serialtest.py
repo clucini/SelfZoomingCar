d724c09a1a615c38c7fdfbce7c1edef95d595249
ubilinux <clucini07@gmail.com>
2019-05-29 06:53:13 +0000

    aaa

--- serialtest.py
+++ serialtest.py
@@ -1,15 +1,13 @@
 import serial
-usbport = 'COM4'
+usbport = '/dev/ttyAMA0'
 ser = serial.Serial(usbport,9600,timeout = 1)
 
 
 def move(angle):
-    s = b''
     ser.write(angle.encode())
-    print(ser.read())
+    print(ser.readline())
 
 while(1):
-    
     a = input("vale")
     move(a)
 
