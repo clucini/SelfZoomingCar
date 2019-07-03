a45a3cfb28b45b960ec1bb5371cbfff36bf76326
Spiritonia <Clucini07@gmail.com>
2019-07-03 15:02:44 +1000

    make green slightly smaller

--- components/checkgreen.py
+++ components/checkgreen.py
@@ -7,10 +7,11 @@ def check(helper, memory):
     green_lower = (55, 40, 40)
     green_upper = (80,255,255)
 
-    green_hsv = helper['hsv'][int(helper['ourLocation'][1].astype(int)/10*5):,]
+    green_hsv = helper['hsv'][int(helper['ourLocation'][1].astype(int)/10*4):,]
 
     green_mask = cv2.inRange(green_hsv, green_lower, green_upper)
-    cv2.imshow('green', green_mask)
+    if memory['debug']:
+        cv2.imshow('green', green_mask)
     
     cnts = cv2.findContours(green_mask, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[0]
     c_count = 0
